89be4d68adcd255b8df60573226bd0e367205ebd
Ronak H Shah <rons.shah@gmail.com>
2015-10-06 10:36:49 -0400

    Bug Fixes

--- bin/RunStructuralVariantPipeline_Delly.pl
+++ bin/RunStructuralVariantPipeline_Delly.pl
@@ -2842,7 +2842,7 @@ sub MergeAllFiles {
 			tie( my %delHash, 'Tie::IxHash' );
 			%delHash = %$delData;
 			foreach my $record ( sort keys %delHash ) {
-				my ( $chr1, $pos1, $chr2, $pos2 ) = split( ":", $record );
+				my ( $chr1, $pos1,$str1, $chr2, $pos2,$str2 ) = split( ":", $record );
 				my (
 					$svType,      $gene1,       $gene2,
 					$transcript1, $transcript2, $site1,
@@ -2861,7 +2861,7 @@ sub MergeAllFiles {
 			tie( my %dupHash, 'Tie::IxHash' );
 			%dupHash = %$dupData;
 			foreach my $record ( sort keys %dupHash ) {
-				my ( $chr1, $pos1, $chr2, $pos2 ) = split( ":", $record );
+				my ( $chr1, $pos1,$str1, $chr2, $pos2,$str2 ) = split( ":", $record );
 				my (
 					$svType,      $gene1,       $gene2,
 					$transcript1, $transcript2, $site1,
@@ -2880,7 +2880,7 @@ sub MergeAllFiles {
 			tie( my %invHash, 'Tie::IxHash' );
 			%invHash = %$invData;
 			foreach my $record ( sort keys %invHash ) {
-				my ( $chr1, $pos1, $chr2, $pos2 ) = split( ":", $record );
+				my ( $chr1, $pos1,$str1, $chr2, $pos2,$str2 ) = split( ":", $record );
 				my (
 					$svType,      $gene1,       $gene2,
 					$transcript1, $transcript2, $site1,
@@ -2899,7 +2899,7 @@ sub MergeAllFiles {
 			tie( my %jmpHash, 'Tie::IxHash' );
 			%jmpHash = %$jmpData;
 			foreach my $record ( sort keys %jmpHash ) {
-				my ( $chr1, $pos1, $chr2, $pos2 ) = split( ":", $record );
+				my ( $chr1, $pos1,$str1, $chr2, $pos2,$str2 ) = split( ":", $record );
 				my (
 					$svType,      $gene1,       $gene2,
 					$transcript1, $transcript2, $site1,
@@ -2999,16 +2999,11 @@ sub Read_dRangerOut {
 	while (<DFH>) {
 		chomp;
 		next if ( $. == 1 );
-		my (
-			$chr1,        $pos1,  $str1,  $chr2,
-			$pos2,        $str2,  $gene1, $site1,
-			$transcript1, $gene2, $site2, $transcript2,
-			$fusion
-		) = split( "\t", $_ );
-		$chr1 = "X" if ( $chr1 eq "23" );
-		$chr2 = "X" if ( $chr2 eq "23" );
-		$chr1 = "Y" if ( $chr1 eq "24" );
-		$chr2 = "Y" if ( $chr2 eq "24" );
+		my ($chr1, $pos1, $str1, $chr2, $pos2, $str2,  $gene1, $site1, $transcript1, $gene2, $site2, $transcript2, $fusion) = split( "\t", $_ );
+		$chr1 = "X" if ( $chr1 =~ /23/ );
+		$chr2 = "X" if ( $chr2 =~ /23/ );
+		$chr1 = "Y" if ( $chr1 =~ /24/ );
+		$chr2 = "Y" if ( $chr2 =~ /24/ );
 		$outHash{"$chr1:$pos1:$str1:$chr2:$pos2:$str2"} =
 "$str1;$str2;$gene1;$gene2;$transcript1;$transcript2;$site1;$site2;$fusion";
 	}
